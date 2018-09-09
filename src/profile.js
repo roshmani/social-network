e7995135f2626b425cbe6b163f13d0bdd0bf25ba
roshni <roshni.mani@gmail.com>
2018-09-09 18:50:51 +0200

    added some css for profile

--- src/profile.js
+++ src/profile.js
@@ -14,25 +14,7 @@ export default class Profile extends Component {
         } = this.props;
 
         return (
-            <div className="Profilediv">
-                <div className="left">
-                    <h1>
-                        {fname} {lname}
-                    </h1>
-
-                    {bio ? (
-                        <div className="biodisplaydiv">
-                            <p className="displayBio">{bio}</p>
-                            <button onClick={toggleBio}>Edit Bio</button>
-                        </div>
-                    ) : (
-                        <p onClick={toggleBio}>Add a Bio</p>
-                    )}
-
-                    {showBio && (
-                        <textarea onKeyDown={setBio} defaultValue={bio} />
-                    )}
-                </div>
+            <aside className="Profilediv">
                 <div className="profileimgdiv">
                     <img
                         className="profileimage"
@@ -40,8 +22,30 @@ export default class Profile extends Component {
                         alt={fname}
                         src={imageUrl}
                     />
+                    <h1>
+                        {fname} {lname}
+                    </h1>
                 </div>
-            </div>
+                <div className="profilebio">
+                    {bio ? (
+                        <div className="biodisplaydiv">
+                            <p className="displayBio">{bio}</p>
+                            <i onClick={toggleBio} className="far fa-edit" />
+                        </div>
+                    ) : (
+                        <p onClick={toggleBio}>Add a Bio</p>
+                    )}
+
+                    {showBio && (
+                        <textarea
+                            className="biotxtarea"
+                            onKeyDown={setBio}
+                            defaultValue={bio}
+                            title="Please write a short bio in 1000 or less words"
+                        />
+                    )}
+                </div>
+            </aside>
         );
     }
 }
